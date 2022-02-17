bbada6cfced9a21798a2e51b3f6fac8f14d0abb2
= <87021923+lvelitoserempre@users.noreply.github.com>
2022-02-17 10:35:13 -0500

    up info

--- static/scripts.js
+++ static/scripts.js
@@ -342,7 +342,7 @@ window.onload = async function () {
         console.log('is this asked?: ',element.currentAsk);
         if(claim) {
           insert = `<div class="relative col-span-3 sm:col-span-2 md:col-span-2 xl:col-span-1 square">
-            <a href="https://startupy.themodernbillboard.com/lot/${name}" target="_blank" class="absolute frame-item w-full h-full flex justify-center items-center bg-white border border-2 border-gray-800">
+            <a href="${lotUrl}" target="_blank" class="absolute frame-item w-full h-full flex justify-center items-center bg-white border border-2 border-gray-800">
               <img src="${element.contentURI}" class="block w-full h-full object-cover" />
             </a>
           </div>`;
@@ -355,7 +355,7 @@ window.onload = async function () {
           </div>`;
         } else {
           insert = `<div class="relative col-span-3 sm:col-span-2 md:col-span-2 xl:col-span-1 square">
-            <a href="${lotUrl}" target="_blank" class="absolute frame-item w-full h-full flex justify-center items-center bg-white border border-2 border-gray-800">${name}</a>
+            <a href="https://startupy.themodernbillboard.com/lot/${name}" target="_blank" class="absolute frame-item w-full h-full flex justify-center items-center bg-white border border-2 border-gray-800">${name}</a>
           </div>`;
         }
         container.insertAdjacentHTML('beforeend',insert);
